bbd9f1595adc8d2e07e510a442da28a08d8e4d95
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-02-10 22:46:54 +0700

    Completed Login Page + Need to check if database connected successfully

--- app.py
+++ app.py
@@ -1,42 +1,113 @@
 import sys
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow,
     QWidget,
+    QStackedLayout,
     QGridLayout,
+    QVBoxLayout,
+    QHBoxLayout,
+    QFormLayout,
     QLabel,
+    QPushButton,
     QLineEdit,
     QTableView,
 )
 from PySide6.QtSql import QSqlDatabase, QSqlQueryModel
 
 
-class DBWindow(QMainWindow):
+class DBApp(QWidget):
+    switchPages = Signal(str, str)
+
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle("Interface DB")
+        self.setWindowTitle("PostgreSQL Interface")
         self.resize(800, 600)
 
-        self.db_display()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.stacked_layout = QStackedLayout()
+        layout.addLayout(self.stacked_layout)
+
+        self.login_screen()
         self.show()
 
-    def db_display(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QGridLayout(central_widget)
+    def login_screen(self):
+        login_page = QWidget()
+        layout = QVBoxLayout()
+        h_title_layout = QHBoxLayout()
+        h_form_layout = QHBoxLayout()
+        form_layout = QFormLayout()
+
+        # Top Down Center
+        layout.addStretch()
+        layout.addLayout(h_title_layout)
+        layout.addLayout(h_form_layout)
+        layout.addStretch()
+
+        # Title Center
+        h_title_layout.addStretch()
+        h_title_layout.addWidget(QLabel("Login Page"))
+        h_title_layout.addStretch()
+        h_title_layout.setContentsMargins(0, 0, 0, 50)
+
+        # Forms Center
+        h_form_layout.addStretch()
+        h_form_layout.addLayout(form_layout)
+        h_form_layout.addStretch()
+
+        username_line = QLineEdit()
+        password_line = QLineEdit()
+        login_button = QPushButton("Login")
+        login_button.setEnabled(False)
+
+        form_layout.addRow("DB Username:", username_line)
+        form_layout.addRow("DB Password:", password_line)
+        form_layout.addWidget(login_button)
+
+        login_page.setLayout(layout)
+        self.stacked_layout.addWidget(login_page)
+
+        # Enable button once forms filled
+        username_line.textChanged.connect(
+            lambda: self.check_forms(username_line, password_line, login_button)
+        )
+        password_line.textChanged.connect(
+            lambda: self.check_forms(username_line, password_line, login_button)
+        )
+
+        login_button.clicked.connect(
+            lambda: self.switchPages.emit(username_line.text(), password_line.text())
+        )
+
+    def check_forms(
+        self,
+        username_line: QLineEdit,
+        password_line: QLineEdit,
+        login_button: QPushButton,
+    ) -> None:
+        if len(username_line.text()) > 0 and len(password_line.text()) > 0:
+            login_button.setEnabled(True)
+        else:
+            login_button.setEnabled(False)
+
+    def switch_pages(self, db_username: str, db_password: str):
+        self.stacked_layout.setCurrentIndex(1)
+        self.db_display(db_username, db_password)
+
+    def db_display(self, db_username: str, db_password: str) -> None:
+        db_page = QWidget()
+        layout = QGridLayout()
 
         layout.addWidget(QLabel("Transactions"), 0, 0)
         layout.addWidget(QLabel("Search:"), 1, 0)
         layout.addWidget(QLineEdit(), 2, 0, 1, 3)
 
-        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.setLayout(layout)
-
         db = QSqlDatabase.addDatabase("QPSQL")
         db.setDatabaseName("postgres")
-        db.setUserName("postgres")
-        db.setPassword("admin10110")
+        db.setUserName(db_username)
+        db.setPassword(db_password)
         db.setHostName("localhost")
         db.setPort(5432)
 
@@ -67,11 +138,16 @@ class DBWindow(QMainWindow):
         table.setModel(query)
         layout.addWidget(table, 3, 0, 1, 3)
 
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        db_page.setLayout(layout)
+        self.stacked_layout.addWidget(db_page)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    with open("styles.qss", "r") as f:
-        _style = f.read()
-        app.setStyleSheet(_style)
-    window = DBWindow()
+    # with open("styles.qss", "r") as f:
+    #     _style = f.read()
+    #     app.setStyleSheet(_style)
+    window = DBApp()
+    window.switchPages.connect(window.switch_pages)
     app.exec()
